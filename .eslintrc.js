a3951027d419aa002affb89777860355a15bd41b
Ryan <627311610@qq.com>
2022-03-22 01:07:25 +0800

    update: publish 0.0.5

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,15 @@ module.exports = {
     'react/react-in-jsx-scope': 0,
     'max-len': 0,
     'react/prop-types': 0,
-    'default-param-last': 0
+    'default-param-last': 0,
+    'react/no-array-index-key': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/no-unstable-nested-components': 0,
+    'no-template-curly-in-string': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'no-plusplus': 0,
+    'import/prefer-default-export': 0,
+    'dot-notation': 0
   }
 };
