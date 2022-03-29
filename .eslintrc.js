3326c328b0faf8f62ba1707f50d66b6d3bc97a57
Ryan <627311610@qq.com>
2022-03-29 22:48:37 +0800

    update: 加入 typescript, version 1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,8 @@ module.exports = {
     'eslint:recommended',
     'plugin:import/errors',
     'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended'
+    'plugin:jsx-a11y/recommended',
+    'plugin:@typescript-eslint/recommended'
   ],
   plugins: ['react-hooks'],
   rules: {
