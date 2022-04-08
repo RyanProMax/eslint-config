28a2846627c0aafe35fe72d9e093244510827966
张宇雁 <zhangyuyan.1024@bytedance.com>
2022-04-08 17:26:45 +0800

    feat: v2.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,50 +1,14 @@
 module.exports = {
-  env: {
-    es6: true,
-    browser: true,
-    node: true
-  },
-  extends: [
-    'airbnb',
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:react/recommended',
-    'plugin:jsx-a11y/recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:vue/vue3-recommended'
-  ],
-  plugins: ['react-hooks'],
+  /**
+   * https://github.com/antfu/eslint-config
+   */
+  extends: ['@antfu'],
   rules: {
+    // comma-dangle never
     'comma-dangle': ['error', 'never'],
-    'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
-    'linebreak-style': 0,
-    quotes: [2, 'single'],
-    'react/react-in-jsx-scope': 0,
-    'max-len': 0,
-    'react/prop-types': 0,
-    'default-param-last': 0,
-    'react/no-array-index-key': 0,
-    'react/jsx-props-no-spreading': 0,
-    'react/no-unstable-nested-components': 0,
-    'no-template-curly-in-string': 0,
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'no-plusplus': 0,
-    'import/prefer-default-export': 0,
-    'dot-notation': 0,
-
-    // ant fu
-    'vue/max-attributes-per-line': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/multi-word-component-names': 'off',
-
-    // reactivity transform
-    'vue/no-setup-props-destructure': 'off',
-
-    'vue/component-tags-order': ['error', {
-      order: ['script', 'template', 'style'],
-    }],
+    '@typescript-eslint/comma-dangle': ['error', 'never'],
+    // semi always
+    'semi': ['error', 'always'],
+    '@typescript-eslint/semi': ['error', 'always']
   }
 };
---
+++ CHANGELOG.md
@@ -0,0 +1,19 @@
+# [2.0.0](https://github.com/RyanProMax/eslint-config/compare/1.1.0...2.0.0) (2022-04-08)
+
+### BREAKING CHANGES
+
+* 完全基于 @antfu/eslint-config 进行个性化配置
+
+
+# [1.1.0](https://github.com/RyanProMax/eslint-config/compare/1.0.0...1.1.0) (2022-04-08)
+
+### Features
+
+* 兼容 vue
+
+
+# 1.0.0 (2022-03-29)
+
+### Features
+
+* 兼容 react
